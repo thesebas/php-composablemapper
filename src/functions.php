c80aa4359371c8fd132a0c123550c6eb9bbaddcd
Sebastian Szymbor <thesebas@users.noreply.github.com>
2019-04-26 15:00:19 +0200

    initial commit

---
+++ src/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace thesebas\composablemapper;
+
+function compose(...$functions)
+{
+    return function ($r) use ($functions) {
+        return array_reduce($functions, function ($res, $func) {
+            return $func($res);
+        }, $r);
+    };
+}
+
+function takeField($fname)
+{
+    return function ($r) use ($fname) {
+        return $r[$fname];
+    };
+}
+
+function nullOn($nullVal)
+{
+    return function ($val) use ($nullVal) {
+        if ($val === $nullVal) {
+            return null;
+        }
+        return $val;
+    };
+}
+
+function ifNotNull(\Closure $param)
+{
+    return function ($val) use ($param) {
+        if ($val === null) {
+            return null;
+        }
+        return $param($val);
+    };
+}
